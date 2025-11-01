9cdb17d24d92682ff6bc39ee34bc4e1646916e09
Muhammad Shaban <shabankinh389@gmail.com>
2025-11-01 18:31:07 +0500

    fix: replace motion with framer-motion for better React compatibility

--- src/components/ParallaxBackground.jsx
+++ src/components/ParallaxBackground.jsx
@@ -1,4 +1,4 @@
-import { motion, useScroll, useSpring, useTransform } from "motion/react";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 
 const ParallaxBackground = () => {
   const { scrollYProgress } = useScroll();
@@ -50,7 +50,7 @@ const ParallaxBackground = () => {
             y: mountain2Y,
           }}
         />
-        {/* Mountaine Layer 1 */}
+        {/* Mountain Layer 1 */}
         <motion.div
           className="absolute inset-0 -z-10"
           style={{
